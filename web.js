ea77b0e15f2e083ced634ba8f09c25caee440bbe
Calvin Davis <hi@calvindavis.co.uk>
2014-06-08 17:08:38 +0100

    Modified templates to be proper Jade templates; Added index and rss routes.

--- web.js
+++ web.js
@@ -4,9 +4,42 @@ var jade = require("jade");
 var app = express();
 var port = Number(process.env.PORT || 5000);
 
-app.get("/", function (req, res, next) {
-	res.set("Content-Type: application/json");
-	res.send(JSON.stringify({ isItFuckingChristmas: "no" }));
-});
+var indexHandler = function (req, res, next) {
+	jade.renderFile(__dirname + "/templates/index.jade", {
+		message: "Hell to the no."
+	}, function (err, html) {
+		if (err) {
+			throw err;
+		}
+		
+		res.set("Content-Type", "text/html");
+		res.send(html);
+	});
+};
+
+app.get("/", indexHandler);
+app.get(/(\d{4}\-\d{2}\-\d{2})/, indexHandler);
+
+var rssHandler = function (req, res, next) {
+	jade.renderFile(__dirname + "/templates/rss.jade", {
+		items: [
+			{
+				title: "No fucking way."
+			},
+			{
+				title: "Heck no dawg."
+			}
+		]
+	}, function (err, html) {
+		if (err) {
+			throw err;
+		}
+		
+		res.set("Content-Type", "application/xml");
+		res.send(html);
+	});
+};
+
+app.get("/rss", rssHandler);
 
 app.listen(port);
